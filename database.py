5cabd0de60f88fd588a874a87d03324c13fc539f
Petr Morávek <moravp15@fit.cvut.cz>
2022-04-26 22:49:42 +0200

    scarp.py, database.py

--- database.py
+++ database.py
@@ -1,4 +1,7 @@
+import os
 import sqlite3
+import sys
+from datetime import datetime
 
 
 class Database:
@@ -14,6 +17,9 @@ class Database:
             away_team TEXT NOT NULL,
             away_team_score INTEGER NOT NULL,
             home_team_score INTEGER NOT NULL,
+            date_day INTEGER NOT NULL,
+            date_month INTEGER NOT NULL,
+            date_year INTEGER NOT NULL,
             PRIMARY KEY (phase, home_team, away_team)
         );""")
         self.db.execute("""CREATE TABLE if not exists match_guess (
@@ -25,52 +31,63 @@ class Database:
             home_team_score INTEGER NOT NULL,
             PRIMARY KEY (nickname_player, phase, home_team, away_team)
         );""")
-        self.conn.commit()
-        self.conn.close()
+        self.__commit_and_close()
 
     def __connect(self):
         self.conn = sqlite3.connect('database.db')
         self.db = self.conn.cursor()
 
-    def add_player(self, nickname, name):
-        self.__connect()
-        self.db.execute("INSERT INTO player VALUES (?, ?)", (nickname, name))
+    def __commit_and_close(self):
         self.conn.commit()
         self.conn.close()
+
+    def add_player(self, nickname, name):
+        self.__connect()
+        try:
+            self.db.execute("INSERT INTO player VALUES (?, ?)", (nickname, name))
+        except sqlite3.IntegrityError:
+            print("Name Alreasy in the database")
+            sys.exit(1)
+        self.__commit_and_close()
+
+    def del_player(self, nickname):
+        self.__connect()
+        self.db.execute("DELETE FROM player WHERE nickname_player=?", (nickname,))
+        self.db.execute("DELETE FROM match_guess WHERE nickname_player=?", (nickname,))
+        self.__commit_and_close()
 
     def add_match_guess(self, name, phase, home_team, away_team, home_team_score, away_team_score):
         self.__connect()
         self.db.execute("INSERT INTO match_guess VALUES (?,?,?,?,?,?)",
                         (name, phase, home_team, away_team, home_team_score, away_team_score))
-        self.conn.commit()
-        self.conn.close()
+        self.__commit_and_close()
 
-    def select_test(self):
+    def add_match_result(self, phase, home_team, away_team, home_team_score, away_team_score, day, month, year):
         self.__connect()
-        self.db.execute("SELECT * FROM player")
-        print(self.db.fetchall())
-        self.db.execute("SELECT * FROM match_guess")
-        print(self.db.fetchall())
-        self.conn.commit()
-        self.conn.close()
+        self.db.execute("INSERT OR IGNORE INTO match_result VALUES (?,?,?,?,?,?,?,?)",
+                        (phase, home_team, away_team, home_team_score, away_team_score, day, month, year))
+        self.__commit_and_close()
 
+    def get_date_of_last_result(self):
+        self.__connect()
+        self.db.execute("SELECT date_day, date_month, date_year FROM match_result ORDER BY date_year DESC, date_month DESC, date_day DESC ")
+        date = self.db.fetchone()
+        self.__commit_and_close()
+        if date is None:
+            return None
+        return datetime(date[2], date[1], date[0])
 
-db = Database()
-db.add_player('Honza', 'F K')
-db.add_player('Petr', 'F G')
-db.add_player('Břéťa', 'R T')
-db.add_match_guess('Honza', 'GS', 'Italy', 'Germany', 2, 1)
-db.add_match_guess('Petr', 'GS', 'Italy', 'Germany', 2, 1)
-db.add_match_guess('Honza', 'GS', 'Sweden', 'Germany', 2, 1)
-db.add_match_guess('Honza', 'FI', 'Italy', 'Germany', 2, 1)
-db.select_test()
-# db.execute("INSERT OR IGNORE INTO match_guess VALUES ('Honza', 'GS', 'Italy', 'Germany', 2, 5)")
-# db.execute("INSERT OR IGNORE INTO match_guess VALUES ('Petr', 'GS', 'Italy', 'Germany', 2, 1)")
-# db.execute("INSERT OR IGNORE INTO match_guess VALUES ('Honza', 'GS', 'Belgium', 'Germany', 2, 1)")
-# db.execute("INSERT OR IGNORE INTO match_guess VALUES ('Honza', 'GS', 'Italy', 'Belgium', 2, 1)")
-#
-# db.execute("SELECT * FROM match_guess WHERE home_team= 'Belgium'")
-# print(db.fetchall())
-#
-# conn.commit()
-# conn.close()
+    def print_all_playes(self):
+        self.__connect()
+        self.db.execute(
+            "SELECT * FROM player")
+        print(self.db.fetchall())
+        self.__commit_and_close()
+
+    def print_all_guesses(self):
+        self.__connect()
+        self.db.execute(
+            "SELECT * FROM match_guess")
+        print(self.db.fetchall())
+        self.__commit_and_close()
+
--- scarp.py
+++ scarp.py
@@ -1,38 +1,81 @@
+import datetime
 import re
 import sys
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.sportinglife.com/football/fixtures-results/competitions/english-fa-cup/76/2022-01"
 
-try:
-    html_content = requests.get(url, timeout=4.0).text
-except requests.exceptions.RequestException as e:
-    print("ERROR network")
-    sys.exit(1)
+class Scarper:
+    request_timeout = 3.0
 
-soup = BeautifulSoup(html_content, "html.parser")
-dates = soup.findAll('li', class_='DatesList__ListItem-sc-1iq29n-0 bVBqIz')
-for date in dates:
-    matches = date.findAll('li', class_='List__ItemWrapper-sc-2hb99l-1 ljzDJb')
-    for match in matches:
-        try:
-            match_live_clock = match.find('div', class_='Item__MatchLiveClock-et8305-3 sekoJ').text
-            # extra time / penalties
-            if match_live_clock == 'AET':
-                details_url = 'https://www.sportinglife.com' + match.find('a')['href']
-                print("AET: \t\t", details_url)
-            elif match_live_clock != 'FT':
-                print('--------PASS---------')
-                pass
+    def __init__(self, url, competition_start, competition_end):
+        self.url = url
+        self.competition_start = competition_start
+        self.competition_end = competition_end
+
+    def update_results_in_database(self, database):
+        most_recent_match_date = database.get_date_of_last_result()
+        if most_recent_match_date is None:
+            from_date = self.competition_start
+        else:
+            from_date = max(most_recent_match_date, self.competition_start)
+        to_date = min(datetime.datetime.now(), self.competition_end)
+        m1 = from_date.month
+        y1 = from_date.year
+        while (y1, m1) <= (to_date.year, to_date.month):
+            self.update_results_in_database_of_month_and_year(database, m1, y1)
+            if m1 + 1 == 13:
+                m1 = 1
+                y1 += 1
             else:
-                score = match.find('span', class_='Item__TeamsModifier-et8305-7 iEeIun').text
-                score = re.findall('[0-9]+', score)
+                m1 += 1
 
-            home_team = match.find('span', class_='Item__TeamA-et8305-6 leKmkN').text
-            away_team = match.find('span', class_='Item__TeamB-et8305-8 bHURVJ').text
-            print(f'{home_team} {score[0]}:{score[1]} {away_team}')
-        except Exception as e:
-                print("ERROR during scarping")
-                sys.exit(2)
+    def update_results_in_database_of_month_and_year(self, database, month, year):
+        try:
+            html_content = requests.get(self.url + '/' + str(year) + '-' + str(str(month).zfill(2)), timeout=Scarper.request_timeout).text
+        except requests.exceptions.RequestException:
+            print("ERROR network")
+            sys.exit(1)
+        soup = BeautifulSoup(html_content, "html.parser")
+        dates = soup.findAll('li', class_='DatesList__ListItem-sc-1iq29n-0 bVBqIz')
+        for date in dates:
+            matches = date.findAll('li', class_='List__ItemWrapper-sc-2hb99l-1 ljzDJb')
+            date_time = date.find('h3').text + ' ' + str(year)
+            datetime_object = datetime.datetime.strptime(date_time, "%A %B %d %Y")
+            today = datetime.datetime.now()
+            if datetime_object > today:
+                break
+            for match in matches:
+                phase = date.find('h4', class_='RoundList__RoundHeader-sc-718t01-0 dJPQKF')
+                if phase is None:
+                    phase = "Group Stage"
+                else:
+                    phase = phase.text
+                match_live_clock = match.find('div', class_='Item__MatchLiveClock-et8305-3 sekoJ')
+                # The match (at least the standard full time) is not finished yet
+                if match_live_clock is None:
+                    continue
+                match_live_clock = match_live_clock.text
+                # extra time / penalties
+                if match_live_clock == 'AET':
+                    details_url = 'https://www.sportinglife.com' + match.find('a')['href']
+                    html_content2 = requests.get(details_url, timeout=4.0).text
+                    soup2 = BeautifulSoup(html_content2, "html.parser")
+                    time_info = soup2.find('span',
+                                           class_='PeriodScores__StyledPeriodScores-sc-6qgbsu-0 cCYRhW').text
+                    scores = re.findall('[0-9]+-[0-9]', time_info)
+                    score = re.findall('[0-9]+', scores[1])
+                    print('AET ', end='')
+                    # FT means Full Time. If the match clock isn't FT nor AET then don't scarp anything
+                elif match_live_clock == 'FT':
+                    score = match.find('span', class_='Item__TeamsModifier-et8305-7 iEeIun').text
+                    score = re.findall('[0-9]+', score)
+                    # Unknown match_live_clock
+                else:
+                    continue
 
+                home_team = match.find('span', class_='Item__TeamA-et8305-6 leKmkN').text
+                away_team = match.find('span', class_='Item__TeamB-et8305-8 bHURVJ').text
+                print(f'{phase}: {home_team} {score[0]}:{score[1]} {away_team},\t{datetime_object}')
+                database.add_match_result(phase, home_team, away_team, score[0], score[1], datetime_object.day,
+                                          datetime_object.month, datetime_object.year)
