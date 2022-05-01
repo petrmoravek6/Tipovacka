7c04be9cd98e0743611b5b3807bbdd4183ade41d
Petr Morávek <moravp15@fit.cvut.cz>
2022-05-01 23:07:36 +0200

    z

--- remove_player_widget.py
+++ remove_player_widget.py
@@ -21,13 +21,14 @@ class RemovePlayerWidget(QtWidgets.QWidget):
         self.verticalLayout.setObjectName("verticalLayout")
         self.remove_btn = QtWidgets.QPushButton(self)
         self.remove_btn.setObjectName("remove_btn")
+        self.remove_btn.clicked.connect(self.remove_btn_clicked)
         self.verticalLayout.addWidget(self.remove_btn)
         self.exit_btn = QtWidgets.QPushButton(self)
         self.exit_btn.setObjectName("exit_btn")
         self.exit_btn.clicked.connect(lambda: self.close())
         self.verticalLayout.addWidget(self.exit_btn)
         self.horizontalLayout.addLayout(self.verticalLayout)
-
+        self.display_list_of_players()
         self.retranslateUi()
         QtCore.QMetaObject.connectSlotsByName(self)
 
@@ -36,3 +37,14 @@ class RemovePlayerWidget(QtWidgets.QWidget):
         self.setWindowTitle(_translate("removePlayerWidget", "Odebrání hráče ze hry"))
         self.remove_btn.setText(_translate("removePlayerWidget", "Smazat hráče"))
         self.exit_btn.setText(_translate("removePlayerWidget", "Odejít"))
+
+    def remove_btn_clicked(self):
+        if self.listWidget.currentItem() is None:
+            return
+        self.game.database.del_player(str(self.listWidget.currentItem().text()))
+        self.display_list_of_players()
+
+    def display_list_of_players(self):
+        self.listWidget.clear()
+        for idx, nickname in enumerate(self.game.database.get_all_players()):
+            self.listWidget.insertItem(idx, nickname[0])
